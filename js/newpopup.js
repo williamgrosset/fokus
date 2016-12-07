b319a2bf13c0459a7d989019c9e382624031b42d
William <will_grosset@hotmail.com>
2016-12-07 13:39:43 -0800

    really ugly list added to popup window

--- js/newpopup.js
+++ js/newpopup.js
@@ -3,4 +3,13 @@ function fokusTab() {
     win.focus()
 }
 
+function documentEvents() {
+    var input = $("input").val();
+    var domainList = document.getElementById("domain");
+    var domain = document.createElement("li"); 
+    domain.innerHTML = input;
+    domainList.appendChild(domain);
+}
+
+document.getElementById('add').addEventListener('click', documentEvents);
 document.getElementById('tab').addEventListener('click', fokusTab);
