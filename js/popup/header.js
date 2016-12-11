4e64be3858dc863fe70449eebd71bd4773638ec5
William <will_grosset@hotmail.com>
2016-12-11 00:20:34 -0800

    broke main popup into 3 components

--- js/popup/header.js
+++ js/popup/header.js
@@ -1,7 +1,14 @@
-import react from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = (
-    <input type='image'
-     src=''>
-);
+class HeaderLink extends React.Component {
+    fokusTab() {
+        var win = window.open("/html/home.html");
+        win.focus()
+    }
+
+    render() {
+       <img src={'https://github.com/williamgrosset/fokus/blob/master/png/fokus_title_128.png?raw=true'} onClick={fokusTab}/>,
+       document.getElementById('header')
+    }
+}
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+browserify js/popup/header.js -t babelify -o js/popup/popup-bandle.js
 browserify js/popup/popup.js -t babelify -o js/popup/popup-bundle.js
