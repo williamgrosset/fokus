f8ad6956577033ae2190ee01d77721735dc25def
William <will_grosset@hotmail.com>
2017-03-13 21:42:30 -0700

    test suite skeleton

---
+++ test/domain-new.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('domain-new', function() {
+    describe('indexof()', function() {
+        it('should return a valid domain', function() {
+            assert.equal(1, [1,2,3].indexOf(1));
+        });
+    });
+});
---
+++ test/toggle.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('toggle', function() {
+    describe('enableDomains()', function() {
+        it('should return a valid domain', function() {
+            assert.equal(1, [1,2,3].indexOf(1));
+        });
+    });
+});
