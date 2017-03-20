0381581ee79f9dbafd4f87d645c32f19a90aa3ed
William <will_grosset@hotmail.com>
2017-03-19 21:03:05 -0700

    domain-item test skeleton

--- test/domain-item.spec.js
+++ test/domain-item.spec.js
@@ -9,23 +9,24 @@ import DomainItem from '../src/js/popup/domain-item.js'
 *  Tests for DomainItem component in src/js/popup/domain-item.js.
 */
 describe('<DomainItem />', function() {
-    it('should have props for domain container and handling addDomain()', function () {
+    it('should have props for the domain, domain container, handling removeDomain()', function () {
         const wrapper = shallow(<DomainItem domain={""}/>);
-        //expect(wrapper.props().addDomain).to.be.defined;
-        //expect(wrapper.props().container).to.be.defined;
+        expect(wrapper.props().container).to.be.defined;
+        expect(wrapper.props().domain).to.be.defined;
+        expect(wrapper.props().removeDomain).to.be.defined;
     });
-    describe('domainValidation(e)', function() {
-        it('should successfully add domain to container', function() {
+    describe('deleteDomain(e) triggered on click', function() {
+        it('should successfully call removeDomain', function() {
             assert(true);
         });
     });
-    describe('domainValidation(e)', function() {
-        it('should show modal for invalid domain', function() {
+    describe('render() with regular domain length', function() {
+        it('should show domain without "..." appended', function() {
             assert(true);
         });
     });
-    describe('domainValidation(e)', function() {
-        it('should show modal for domain container max', function() {
+    describe('render() with long domain length', function() {
+        it('should show domain with "..." appended', function() {
             assert(true);
         });
     });
