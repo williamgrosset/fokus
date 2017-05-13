70b44684549d76ce5e317af628b5067ab87a61f3
William Grosset <will_grosset@hotmail.com>
2017-05-13 15:04:14 -0700

    update about CSS/html/js

--- src/js/home/about.js
+++ src/js/home/about.js
@@ -1,20 +1,19 @@
 (function() {
+  document.addEventListener('DOMContentLoaded', function() {
+    typeHeader();
+  });
 
-    document.addEventListener('DOMContentLoaded', function() {
-          typeHeader();
+  /*
+  *  typed.js: wwww.mattboldt.com
+  *  Type out header on page load.
+  */
+  function typeHeader() {
+    $(".center-above").typed({
+      strings: ["Coming soon."],
+      contentType: "html",
+      showCursor: false,
+      typeSpeed: 90,
+      backSpeed: 90,
     });
-
-    /*
-    *  typed.js: wwww.mattboldt.com
-    *  Type out header on page load.
-    */
-    function typeHeader() {
-        $("#center-above").typed({
-            strings: ["Coming soon."],
-            contentType: "html",
-            showCursor: false,
-            typeSpeed: 90,
-            backSpeed: 90,
-        });
-    }
+  }
 })();
