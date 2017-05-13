bb2a6efef5c70810322d3cbe347210c4a2d2e34a
William Grosset <will_grosset@hotmail.com>
2017-05-13 16:55:49 -0700

    reorder about/home CSS && 2 tab width for listener.js

--- src/js/background/listener.js
+++ src/js/background/listener.js
@@ -1,71 +1,70 @@
 (function() {
+  var domains = [];
+  var domainsEnable = [];
+  var disabled = false;
 
-    var domains = [];
-    var domainsEnable = [];
-    var disabled = false;
-
-    chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
-            // Add domain to domain blocker container
-            if (request.validDomain) {
-                domains.push(request.validDomain);
-                domainsEnable = domains;
-            }
-            // Remove domain from domain blocker container
-            if (request.index) {
-                domains.splice(request.index, 1);
-                domainsEnable = domains;
-            }
-            // Remove domain from domain blocker container
-            if (request.index == 0) {
-                domainsEnable = domains;
-                domains.splice(request.index, 1);
-            }
-            // Disable domain blocker
-            if (request.disable) {
-                if (!disabled) {
-                    var domainsDisable = [];
-                    domainsEnable = domains;
-                    domains = domainsDisable;
-                    disabled = true;
-                }
-            }
-            // Enable domain blocker
-            if (request.enable) {
-                if (disabled) {
-                    domains = domainsEnable;
-                    disabled = false;
-                }
-            }
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      // Add domain to domain blocker container
+      if (request.validDomain) {
+        domains.push(request.validDomain);
+        domainsEnable = domains;
+      }
+      // Remove domain from domain blocker container
+      if (request.index) {
+        domains.splice(request.index, 1);
+        domainsEnable = domains;
+      }
+      // Remove domain from domain blocker container
+      if (request.index == 0) {
+        domainsEnable = domains;
+        domains.splice(request.index, 1);
+      }
+      // Disable domain blocker
+      if (request.disable) {
+        if (!disabled) {
+          var domainsDisable = [];
+          domainsEnable = domains;
+          domains = domainsDisable;
+          disabled = true;
         }
-    );
-
-    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {  
-        if (changeInfo.status == 'complete') {
-            // Check current tab for URL replacement
-            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-                for (var i = 0; i < domains.length; i++) {
-                    var reDomain = new RegExp(domains[i], 'i');
-                    if (reDomain.test(tabs[0].url)) {
-                        chrome.tabs.update(tabs[0].id, {url: "src/html/home.html" });
-                        return;
-                    }
-                }
-            });
-
-            // Check rest of tabs for URL replacement
-            chrome.tabs.query({currentWindow: true}, function (tabs) {
-                var currDomain = location.href;
-                for (var i = 0; i < domains.length; i++) {
-                    for (var k = 0; k < tabs.length; k++) {
-                        var reDomain = new RegExp(domains[i], 'i');
-                        if (reDomain.test(tabs[k].url)) {
-                            chrome.tabs.update(tabs[k].id, {url: "src/html/home.html" });
-                            return;
-                        }
-                    }
-                }       
-            });
+      }
+      // Enable domain blocker
+      if (request.enable) {
+        if (disabled) {
+          domains = domainsEnable;
+          disabled = false;
         }
-    });
+      }
+    }
+  );
+
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {  
+    if (changeInfo.status == 'complete') {
+      // Check current tab for URL replacement
+      chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        for (var i = 0; i < domains.length; i++) {
+          var reDomain = new RegExp(domains[i], 'i');
+          if (reDomain.test(tabs[0].url)) {
+            chrome.tabs.update(tabs[0].id, {url: "src/html/home.html" });
+            return;
+          }
+        }
+      });
+
+      // Check rest of tabs for URL replacement
+      chrome.tabs.query({currentWindow: true}, function (tabs) {
+        var currDomain = location.href;
+        for (var i = 0; i < domains.length; i++) {
+          for (var k = 0; k < tabs.length; k++) {
+            var reDomain = new RegExp(domains[i], 'i');
+            if (reDomain.test(tabs[k].url)) {
+              chrome.tabs.update(tabs[k].id, {url: "src/html/home.html" });
+              return;
+            }
+          }
+        }       
+      });
+    }
+  });
 })();
