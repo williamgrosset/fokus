9a26187ea48c6bc3ae055a4ed985de716d9a0a5d
William Grosset <will_grosset@hotmail.com>
2017-06-08 23:57:05 -0700

    Fix lint errors in Domains component

--- src/js/popup/components/Domains.react.jsx
+++ src/js/popup/components/Domains.react.jsx
@@ -1,51 +1,20 @@
 import React from 'react';
+import shortid from 'shortid';
 import DomainNew from './DomainNew.react.jsx';
 import DomainContainer from './DomainContainer.react.jsx';
-import DomainItem from './DomainItem.react.jsx';
-import shortid from 'shortid';
 
 export default class Domains extends React.Component {
   constructor(props) {
     super(props);
+    this.getIndex = this.getIndex.bind(this);
+    this.removeDomain = this.removeDomain.bind(this);
     this.addDomain = this.addDomain.bind(this);
     this.storeDomain = this.storeDomain.bind(this);
-    this.removeDomain = this.removeDomain.bind(this);
-    this.getIndex = this.getIndex.bind(this);
 
     const container = JSON.parse(localStorage.getItem('container')) || [];
     this.state = {
-      container
+      container,
     };
-  }
-
-  /*
-  *  Adds valid domain from form input into the domain container.
-  *
-  *  @param domain: Domain from form input value.
-  */
-  addDomain(domain) {
-    domain = '.*:\/\/\.*'.concat(domain).concat('\/.*');
-    const newDomain = {
-      id: shortid.generate(),
-      domain
-    };
-    this.setState((prevState) => ({ container: prevState.container.concat(newDomain) }), () => {
-      this.storeDomain(domain, this.state.container);
-    });
-  }
-
-  /*
-  *  Send validDomain to the background script to be added to the collection of blocked domains and store the domains 
-  *  container in HTML localStorage.
-  *
-  *  @param validDomain: Domain with prefix and suffix for proper URL blocking.
-  *  @param container:   Container with all of our domains.
-  */
-  storeDomain(validDomain, container) {
-    chrome.runtime.sendMessage({
-      validDomain
-    });
-    localStorage.setItem('container', JSON.stringify(container));
   }
 
   /*
@@ -65,23 +34,51 @@ export default class Domains extends React.Component {
   }
 
   /*
-  *  Search for index and filter matched item out of container and send index to background script for domain to 
+  *  Search for index and filter matched item out of container and send index to background script for domain to
   *  be removed.
   *
   *  @param id: Unique id for domain to delete.
   */
   removeDomain(id) {
     const index = this.getIndex(id, 'id');
-    if (index === -1) {
-      return;
-    }
+    if (index === -1) return;
 
     chrome.runtime.sendMessage({
-      index
+      index,
     });
     this.setState((prevState) => ({ container: prevState.container.filter((_, ind) => ind !== index) }), () => {
       localStorage.setItem('container', JSON.stringify(this.state.container));
     });
+  }
+
+  /*
+  *  Adds valid domain from form input into the domain container.
+  *
+  *  @param domain: Domain from form input value.
+  */
+  addDomain(domain) {
+    domain = '.*:\/\/\.*'.concat(domain).concat('\/.*');
+    const newDomain = {
+      id: shortid.generate(),
+      domain,
+    };
+    this.setState((prevState) => ({ container: prevState.container.concat(newDomain) }), () => {
+      this.storeDomain(domain, this.state.container);
+    });
+  }
+
+  /*
+  *  Send validDomain to the background script to be added to the collection of blocked domains and store the domains
+  *  container in HTML localStorage.
+  *
+  *  @param validDomain: Domain with prefix and suffix for proper URL blocking.
+  *  @param container:   Container with all of our domains.
+  */
+  storeDomain(validDomain, container) {
+    chrome.runtime.sendMessage({
+      validDomain,
+    });
+    localStorage.setItem('container', JSON.stringify(container));
   }
 
   render() {
