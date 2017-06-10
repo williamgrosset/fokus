019ae63b33eeaf4fe6e9e4475e237d3524432e53
William Grosset <will_grosset@hotmail.com>
2017-06-10 11:19:30 -0700

    Static methods for DomainNew component

--- src/js/popup/components/DomainNew.react.jsx
+++ src/js/popup/components/DomainNew.react.jsx
@@ -3,6 +3,39 @@ import PropTypes from 'prop-types';
 import $ from 'jquery';
 
 export default class DomainNew extends React.Component {
+
+  /*
+  *  Checks if domain is valid before being added into parent container.
+  *
+  *  @param domain:    Domain from form input value.
+  *  @returns boolean: False if domain is invalid, otherwise true.
+  */
+  static errorCheck(domain) {
+    if (domain.includes('http') || domain.includes(':') || domain.includes('/') || domain === ''
+        || !domain.includes('.') || domain.includes(' ')) {
+      return false;
+    }
+    return true;
+  }
+
+  /*
+  *  Displays appropriate modal for max container and domain validation.
+  *
+  *  @param e:  Submitted form input value.
+  *  @param id: ID of modal (valid values: 'myModalMax' or 'myModalError').
+  *  @param i:  Integer value for grabbing appropriate modal within span (valid values: 0 or 1).
+  */
+  static showModal(e, id, i) {
+    const span = $('.close')[i];
+    e.preventDefault();
+
+    $(`#${id}`).css('display', 'block');
+    span.onclick = () => {
+      $(`#${id}`).css('display', 'none');
+    };
+    $('#input').val('');
+  }
+
   constructor(props) {
     super(props);
     this.inputChange = this.inputChange.bind(this);
@@ -46,38 +79,6 @@ export default class DomainNew extends React.Component {
       this.props.addDomain(this.state.value);
       this.setState({ value: '' });
     }
-  }
-
-  /*
-  *  Checks if domain is valid before being added into parent container.
-  *
-  *  @param domain:    Domain from form input value.
-  *  @returns boolean: False if domain is invalid, otherwise true.
-  */
-  errorCheck(domain) {
-    if (domain.includes('http') || domain.includes(':') || domain.includes('/') || domain === ''
-        || !domain.includes('.') || domain.includes(' ')) {
-      return false;
-    }
-    return true;
-  }
-
-  /*
-  *  Displays appropriate modal for max container and domain validation.
-  *
-  *  @param e:  Submitted form input value.
-  *  @param id: ID of modal (valid values: 'myModalMax' or 'myModalError').
-  *  @param i:  Integer value for grabbing appropriate modal within span (valid values: 0 or 1).
-  */
-  showModal(e, id, i) {
-    const span = $('.close')[i];
-    e.preventDefault();
-
-    $(`#${id}`).css('display', 'block');
-    span.onclick = () => {
-      $(`#${id}`).css('display', 'none');
-    };
-    $('#input').val('');
   }
 
   render() {
