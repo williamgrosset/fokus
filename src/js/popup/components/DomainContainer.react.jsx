730a07b5a516a0026e7331b66b8f5542745fc3f7
William Grosset <will_grosset@hotmail.com>
2017-06-10 22:02:06 -0700

    Recreate DomainContainer into stateless function

--- src/js/popup/components/DomainContainer.react.jsx
+++ src/js/popup/components/DomainContainer.react.jsx
@@ -2,33 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DomainItem from './DomainItem.react.jsx';
 
-export default class DomainContainer extends React.Component {
-  render() {
-    // fix this...
-    const removeDomain = this.props;
-    if (this.props.container.length !== 0) {
-      return (
-        <ul id='domain-container'>
-          {this.props.container.map(validDomain => <DomainItem {...removeDomain} {...validDomain} key={validDomain.id} />)}
-        </ul>
-      );
-    }
-    return (
-      <ul id='domain-container'>
-        <p>No domains currently blocked.</p>
-      </ul>
-    );
-  }
-}
-
-/*
-const DomainContainer = ({ container, removeDomain }) => (
-  <ul id='domain-container'>
-    {container.map(domain => <DomainItem {...removeDomain} {...domain} key={domain.id} />)}
-  </ul>
+const DomainContainer = (props) => (
+  props.container.length !== 0 ?
+    <ul id='domain-container'>
+      {props.container.map((validDomain, id) => <DomainItem {...validDomain} {...props} key={id} />)}
+    </ul>
+    :
+    <ul id='domain-container'>
+      <p>No domains currently blocked.</p>
+    </ul>
 );
-*/
 
 DomainContainer.propTypes = {
   container: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
+
+export default DomainContainer;
