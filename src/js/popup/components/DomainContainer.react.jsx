26c4f26179352fcb9722eb3c31b04f0b0e267195
William Grosset <will_grosset@hotmail.com>
2017-06-10 22:27:39 -0700

    Static methods in Toggle component

--- src/js/popup/components/DomainContainer.react.jsx
+++ src/js/popup/components/DomainContainer.react.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DomainItem from './DomainItem.react.jsx';
 
-const DomainContainer = (props) => (
+const DomainContainer = props => (
   props.container.length !== 0 ?
     <ul id='domain-container'>
-      {props.container.map((validDomain, id) => <DomainItem {...validDomain} {...props} key={id} />)}
+      {props.container.map((validDomain, id) => (
+        <DomainItem
+          {...validDomain}
+          {...props}
+          key={id}
+        />),
+      )}
     </ul>
     :
     <ul id='domain-container'>
--- src/js/popup/components/Toggle.react.jsx
+++ src/js/popup/components/Toggle.react.jsx
@@ -2,11 +2,50 @@ import React from 'react';
 import $ from 'jquery';
 
 export default class Toggle extends React.Component {
+
+  /*
+  *  Modify color of domain container items, domain title, and bottom border of form input.
+  *
+  *  @param color: Modify color of domain container items.
+  */
+  static modifyCss(color) {
+    $('#domain-container').css('color', color);
+    $('.domains-title').css('color', color);
+    $('input[type=text]').css('border-bottom-color', color);
+  }
+
+  /*
+  *  Update HTML for enable mode and modify CSS for enabled domain blocker.
+  */
+  static onloadEnable() {
+    const toggle = localStorage.getItem('fokus-toggle');
+    if (toggle === 'disable') {
+      Toggle.modifyCss('#A1A1A1');
+      $('#input').prop('disabled', true);
+      return { __html: 'Enable' };
+    }
+    Toggle.modifyCss('#000000');
+    $('#input').prop('disabled', false);
+    return { __html: 'Enabled' };
+  }
+
+  /*
+  *  Update HTML for disable mode and modify CSS for disabled domain blocker.
+  */
+  static onloadDisable() {
+    const toggle = localStorage.getItem('fokus-toggle');
+    if (toggle === 'disable') {
+      Toggle.modifyCss('#A1A1A1');
+      $('#input').prop('disabled', true);
+      return { __html: 'Disabled' };
+    }
+    Toggle.modifyCss('#000000');
+    $('#input').prop('disabled', false);
+    return { __html: 'Disable' };
+  }
+
   constructor(props) {
     super(props);
-    this.modifyCss = this.modifyCss.bind(this);
-    this.onloadEnable = this.onloadEnable.bind(this);
-    this.onloadDisable = this.onloadDisable.bind(this);
     this.enableFokus = this.enableFokus.bind(this);
     this.disableFokus = this.disableFokus.bind(this);
 
@@ -15,47 +54,6 @@ export default class Toggle extends React.Component {
     this.state = {
       enable,
     };
-  }
-
-  /*
-  *  Update HTML for enable mode and modify CSS for enabled domain blocker.
-  */
-  onloadEnable() {
-    const toggle = localStorage.getItem('fokus-toggle');
-    if (toggle === 'disable') {
-      this.modifyCss('#A1A1A1');
-      $('#input').prop('disabled', true);
-      return { __html: 'Enable' };
-    }
-    this.modifyCss('#000000');
-    $('#input').prop('disabled', false);
-    return { __html: 'Enabled' };
-  }
-
-  /*
-  *  Update HTML for disable mode and modify CSS for disabled domain blocker.
-  */
-  onloadDisable() {
-    const toggle = localStorage.getItem('fokus-toggle');
-    if (toggle === 'disable') {
-      this.modifyCss('#A1A1A1');
-      $('#input').prop('disabled', true);
-      return { __html: 'Disabled' };
-    }
-    this.modifyCss('#000000');
-    $('#input').prop('disabled', false);
-    return { __html: 'Disable' };
-  }
-
-  /*
-  *  Modify color of domain container items, domain title, and bottom border of form input.
-  *
-  *  @param color: Modify color of domain container items.
-  */
-  modifyCss(color) {
-    $('#domain-container').css('color', color);
-    $('.domains-title').css('color', color);
-    $('input[type=text]').css('border-bottom-color', color);
   }
 
   /*
@@ -70,7 +68,7 @@ export default class Toggle extends React.Component {
         });
       });
 
-      this.modifyCss('#000000');
+      Toggle.modifyCss('#000000');
       $('#enable').html('Enabled');
       $('#disable').html('Disable');
       $('#input').prop('disabled', false);
@@ -90,7 +88,7 @@ export default class Toggle extends React.Component {
         });
       });
 
-      this.modifyCss('#A1A1A1');
+      Toggle.modifyCss('#A1A1A1');
       $('#disable').html('Disabled');
       $('#enable').html('Enable');
       $('#input').prop('disabled', true);
@@ -105,14 +103,14 @@ export default class Toggle extends React.Component {
           id='enable'
           className='toggle-button1'
           onClick={this.enableFokus}
-          dangerouslySetInnerHTML={this.onloadEnable()}
+          dangerouslySetInnerHTML={Toggle.onloadEnable()}
         />
         <div className='divider' />
         <p
           id='disable'
           className='toggle-button2'
           onClick={this.disableFokus}
-          dangerouslySetInnerHTML={this.onloadDisable()}
+          dangerouslySetInnerHTML={Toggle.onloadDisable()}
         />
       </div>
     );
