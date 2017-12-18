1a4859bc9a4bd06ea50ae73105fa54221b3ad6ec
William Grosset <williamhgrosset@gmail.com>
2017-12-17 21:43:40 -0800

    Only redirect current tab

--- src/js/background/listener.js
+++ src/js/background/listener.js
@@ -29,26 +29,12 @@
 
   chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
     if (changeInfo.status === 'complete') {
-      // Check current tab for URL replacement
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         for (let i = 0; i < domains.length; i++) {
           const reDomain = new RegExp(domains[i].validDomain, 'i');
           if (reDomain.test(tabs[0].url)) {
             chrome.tabs.update(tabs[0].id, { url: 'src/html/home.html' });
             return;
-          }
-        }
-      });
-
-      // Check rest of tabs for URL replacement
-      chrome.tabs.query({ currentWindow: true }, (tabs) => {
-        for (let i = 0; i < domains.length; i++) {
-          for (let k = 0; k < tabs.length; k++) {
-            const reDomain = new RegExp(domains[i].validDomain, 'i');
-            if (reDomain.test(tabs[k].url)) {
-              chrome.tabs.update(tabs[k].id, { url: 'src/html/home.html' });
-              return;
-            }
           }
         }
       });
