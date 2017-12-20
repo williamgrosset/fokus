f21f59d1c06c13bda080099577837acaf5674e44
William Grosset <williamhgrosset@gmail.com>
2017-12-19 22:25:12 -0800

    Remove state from DomainItem

--- src/js/popup/components/DomainItem.jsx
+++ src/js/popup/components/DomainItem.jsx
@@ -1,55 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class DomainItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.deleteDomain = this.deleteDomain.bind(this);
-
-    this.state = {
-      validDomain: this.props.validDomain || '',
-    };
-  }
-
-  deleteDomain(e) {
+const DomainItem = ({ id, validDomain, removeDomain }) => {
+  const deleteDomain = (e) => {
     e.preventDefault();
-
-    const { id, removeDomain } = this.props;
-    this.setState({ validDomain: '' });
     removeDomain(id);
+  };
+
+  let uiDomain = validDomain;
+  uiDomain = uiDomain.replace('.*://.*', '').replace('/.*', '');
+  // Add "..." to end of the domain if length is too large
+  if (uiDomain.length >= 19) {
+    uiDomain = uiDomain.substring(0, 21).concat('...');
   }
 
-  render() {
-    const { validDomain } = this.state;
-    let uiDomain = validDomain.replace('.*://.*', '').replace('/.*', '');
-
-    // Add "..." to end of the domain if length is too large
-    if (uiDomain.length >= 19) {
-      uiDomain = uiDomain.substring(0, 21).concat('...');
-    }
-
-    return (
-      <li id="domain-item">
-        <div
-          id="domain-name"
-          className="domain-name"
-        >
-          {uiDomain}
-        </div>
-        <div>
-          <input
-            type="image"
-            id="domain-delete"
-            className="garbage-can"
-            alt="Delete"
-            src="/png/garbage_can_16.png"
-            onClick={this.deleteDomain}
-          />
-        </div>
-      </li>
-    );
-  }
-}
+  return (
+    <li id="domain-item">
+      <div
+        id="domain-name"
+        className="domain-name"
+      >
+        {uiDomain}
+      </div>
+      <div>
+        <input
+          type="image"
+          id="domain-delete"
+          className="garbage-can"
+          alt="Delete"
+          src="/png/garbage_can_16.png"
+          onClick={deleteDomain}
+        />
+      </div>
+    </li>
+  );
+};
 
 DomainItem.propTypes = {
   id: PropTypes.string.isRequired,
