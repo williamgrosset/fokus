7fde665de26092f398d82b8c4154f74eba84f6b8
William Grosset <williamhgrosset@gmail.com>
2017-12-19 21:51:33 -0800

    Remove unnecessary div in Header

--- src/js/popup/components/Header.jsx
+++ src/js/popup/components/Header.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
 const Header = () => (
-  <div className="fokus-link">
-    <input
-      type="image"
-      src="/png/fokus_title_128.png"
-      alt="fokus"
-      style={{ outline: "none" }}
-      onClick={() => window.open("/src/html/home.html").focus()}
-    />
-  </div>
+  <input
+    className="fokus-link"
+    type="image"
+    src="/png/fokus_title_128.png"
+    alt="fokus"
+    onClick={() => window.open("/src/html/home.html").focus()}
+  />
 );
 
 export default Header;
