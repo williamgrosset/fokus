6001de06f9caf5f58c1b68776270e9ec2f5d6411
William Grosset <williamhgrosset@gmail.com>
2017-12-20 21:24:42 -0800

    Cleanup DomainNew component

--- src/js/popup/components/DomainNew.jsx
+++ src/js/popup/components/DomainNew.jsx
@@ -6,7 +6,7 @@ class DomainNew extends React.Component {
   constructor(props) {
     super(props);
     this.inputChange = this.inputChange.bind(this);
-    this.domainValidation = this.domainValidation.bind(this);
+    this.saveDomain = this.saveDomain.bind(this);
 
     this.state = {
       value: '',
@@ -14,23 +14,8 @@ class DomainNew extends React.Component {
   }
 
   /*
-  *  Checks if domain is valid before being added into parent container.
-  *  TODO: Check domain validation with regex.
+  *  Displays appropriate modal for container max and domain validation.
   *
-  *  @param domain:    Domain from form input value.
-  *  @returns boolean: False if domain is invalid, otherwise true.
-  */
-  static errorCheck(domain) {
-    if (!/^[a-z0-9]+\.[a-z0-9]+$/.test(domain)) {
-      return true;
-    }
-    return false;
-  }
-
-  /*
-  *  Displays appropriate modal for max container and domain validation.
-  *
-  *  @param e:  Submitted form input value.
   *  @param id: ID of modal (valid values: 'myModalMax' or 'myModalError').
   *  @param i:  Integer value for grabbing appropriate modal within span (valid values: 0 or 1).
   */
@@ -45,54 +30,43 @@ class DomainNew extends React.Component {
     $('#input').val('');
   }
 
-  /*
-  *  Keeps track of the user input and updates state.
-  *
-  *  @param e: Event handler for form input value.
-  */
+  static isDomainValid(domain) {
+    if (!/^[a-z0-9]+\.[a-z0-9]+$/.test(domain)) return true;
+    return false;
+  }
+
+  saveDomain(e) {
+    const domain = this.state.value.toLowerCase();
+    e.preventDefault();
+
+    // Check domain container max
+    if (this.props.container.length === 90) {
+      DomainNew.showModal(e, 'myModalMax', 0);
+      return;
+    }
+
+    // Check if domain is valid
+    if (DomainNew.isDomainValid(domain)) {
+      DomainNew.showModal(e, 'myModalError', 1);
+    // Successfully add domain to be blocked 
+    } else {
+      this.props.addDomain(this.state.value);
+      this.setState({ value: '' });
+    }
+  }
+
   inputChange(e) {
     this.setState({ value: e.target.value });
   }
 
-  /*
-  *  Successfully saves new domain to domain container and resets state.
-  */
-  saveDomain() {
-    this.props.addDomain(this.state.value);
-    this.setState({ value: '' });
-  }
-
-  /*
-  *  Add valid domain to domain container or show appropriate modal if an error exists.
-  *
-  *  @param e: Event handler for form input value.
-  */
-  domainValidation(e) {
-    const domain = this.state.value.toLowerCase();
-    e.preventDefault();
-
-    // Show modal for domain container max
-    if (this.props.container.length === 30) {
-      DomainNew.showModal(e, 'myModalMax', 0);
-      return;
-    }
-    // Show modal for invalid domain
-    if (DomainNew.errorCheck(domain)) {
-      DomainNew.showModal(e, 'myModalError', 1);
-    // Successfully add domain to domain blocker container
-    } else {
-      this.saveDomain();
-    }
-  }
-
   render() {
     return (
-      <form id='form' autoComplete='off' onSubmit={this.domainValidation}>
+      <form id="form" autoComplete="off" onSubmit={this.saveDomain}>
         <input
-          id='input'
-          type='text'
-          placeholder='e.g. tomgrosset.com'
-          autoComplete='off'
+          id="input"
+          type="text"
+          placeholder="e.g. tomgrosset.com"
+          autoComplete="off"
           value={this.state.value}
           onChange={this.inputChange}
         />
