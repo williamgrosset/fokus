340ec13426f42a46b0984c58a88e00c4adf7a46c
William Grosset <williamhgrosset@gmail.com>
2017-12-20 22:21:11 -0800

    Wtf was I thinking

--- src/js/popup/components/Toggle.jsx
+++ src/js/popup/components/Toggle.jsx
@@ -6,33 +6,18 @@ class Toggle extends React.Component {
     super(props);
     this.enableFokus = this.enableFokus.bind(this);
     this.disableFokus = this.disableFokus.bind(this);
-    this.loadEnableOrDisableMode = this.loadEnableOrDisableMode.bind(this);
 
     this.state = {
       enabled: localStorage.getItem('fokus-toggle') === 'enable' || false,
     };
   }
 
-  static modifyCss(color) {
+  static modifyCss(enabled) {
+    const color = enabled ? '#000000' : '#A1A1A1';
     $('.domain-container').css('color', color);
     $('.domains-title').css('color', color);
     $('input[type=text]').css('border-bottom-color', color);
-  }
-
-  loadEnableOrDisableMode(enable) {
-    if (this.state.enabled === false) {
-      Toggle.modifyCss('#A1A1A1');
-      $('#input').prop('disabled', true);
-
-      if (!enable) return { __html: 'Enable' };
-      return { __html: 'Disabled' };
-    }
-
-    Toggle.modifyCss('#000000');
-    $('#input').prop('disabled', false);
-
-    if (enable) return { __html: 'Enabled' };
-    return { __html: 'Disable' };
+    $('#input').prop('disabled', !enabled);
   }
 
   /*
@@ -40,19 +25,12 @@ class Toggle extends React.Component {
   *  Update boolean value in localStorage and modify CSS for enabled visuals.
   */
   enableFokus() {
-    if (this.state.enabled === false) {
-      this.setState({ enabled: true }, () => {
-        chrome.runtime.sendMessage({
-          toggle: true,
-        });
+    localStorage.setItem('fokus-toggle', 'enable');
+    this.setState({ enabled: true }, () => {
+      chrome.runtime.sendMessage({
+        toggle: true,
       });
-
-      Toggle.modifyCss('#000000');
-      $('#enable').html('Enabled');
-      $('#disable').html('Disable');
-      $('#input').prop('disabled', false);
-      localStorage.setItem('fokus-toggle', 'enable');
-    }
+    });
   }
 
   /*
@@ -60,37 +38,35 @@ class Toggle extends React.Component {
   *  Update boolean value in localStorage and modify CSS for disabled visuals.
   */
   disableFokus() {
-    if (this.state.enabled === true) {
-      this.setState({ enabled: false }, () => {
-        chrome.runtime.sendMessage({
-          toggle: false,
-        });
+    localStorage.setItem('fokus-toggle', 'disable');
+    this.setState({ enabled: false }, () => {
+      chrome.runtime.sendMessage({
+        toggle: false,
       });
-
-      Toggle.modifyCss('#A1A1A1');
-      $('#disable').html('Disabled');
-      $('#enable').html('Enable');
-      $('#input').prop('disabled', true);
-      localStorage.setItem('fokus-toggle', 'disable');
-    }
+    });
   }
 
   render() {
+    const { enabled } = this.state;
+    Toggle.modifyCss(enabled);
+
     return (
       <div className='toggle'>
         <button
           id='enable'
           className='toggle-button'
           onClick={this.enableFokus}
-          dangerouslySetInnerHTML={this.loadEnableOrDisableMode(this.state.enabled)}
-        />
+        >
+          {enabled ? 'Enabled' : 'Enable'}
+        </button>
         <div className='divider' />
         <button
           id='disable'
           className='toggle-button'
           onClick={this.disableFokus}
-          dangerouslySetInnerHTML={this.loadEnableOrDisableMode(!this.state.enabled)}
-        />
+        >
+          {enabled ? 'Disable' : 'Disabled'}
+        </button>
       </div>
     );
   }
